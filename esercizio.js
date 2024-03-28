7fcad1c12a786c51d12c075738b71e982380e479
Mirko Vitale <mirko.vitale.1997@outlook.com>
2024-03-28 12:47:46 +0100

    creazione team1.js

--- esercizio.js
+++ esercizio.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+const team1 = [{
+    name: "Mirko",
+    surname: "Vitale",
+    age: 26,
+    city: "Rome",
+    hobby: "Music",
+    favoriteFood: "Carbonara",
+    favoriteVideoGame: "Call of Duty",
+    favoriteFilm: "Django",
+    favoriteBook: "Metro 2033",
+    petName: "Melody"
+}
+]
