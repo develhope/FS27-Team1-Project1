440db46706d9c1f0747d4a120cd330875cd9fa3c
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-03-28 12:30:01 +0100

    feat: implemented object

--- esercizio.js
+++ esercizio.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const person = [
+    { name: "Andrea",
+    surname: "Schiariti",
+    age: 33,
+    city: "Rome",
+    hobby: "Playing videgames",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Silent Hill",
+    favoriteFilm: "Mrs Doubtfire",
+    favoriteBook: "The Stone Monkey",
+    petName:"Guy",}
+]
