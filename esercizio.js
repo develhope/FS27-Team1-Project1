510b0a084b40d663d656dbe2e4a4eabed2042109
Domenico Provenzano <puledroimpennatopub@yahoo.com>
2024-03-28 12:31:00 +0100

    creato array

--- esercizio.js
+++ esercizio.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const team1 = [{
+    name: "Domenico",
+    surname: "Provenzano",
+    age: 33,
+    city: "Bagheria",
+    hobby: "karate",
+    favoriteFood: "Hamburger",
+    favoriteVideoGame: "League of Legends",
+    favoriteFilm: "Il buono, il brutto e il cattivo",
+    favoriteBook: "Il mastino dei Baskerville",
+    petName: "Stella"
+}];
