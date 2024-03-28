7d87a8bfe36297a0bce94217a4d787a87075435b
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-03-28 16:27:56 +0100

    feat: surname in alphabetical order implemented

--- esercizio.js
+++ esercizio.js
@@ -1,4 +1,5 @@
-const team1 = [{
+const team1 = [
+  {
     name: "Domenico",
     surname: "Provenzano",
     age: 33,
@@ -8,9 +9,9 @@ const team1 = [{
     favoriteVideoGame: "League of Legends",
     favoriteFilm: "Il buono, il brutto e il cattivo",
     favoriteBook: "Il mastino dei Baskerville",
-    petName: "Stella"
-},
-{
+    petName: "Stella",
+  },
+  {
     name: "Mirko",
     surname: "Vitale",
     age: 26,
@@ -20,9 +21,9 @@ const team1 = [{
     favoriteVideoGame: "Call of Duty",
     favoriteFilm: "Django",
     favoriteBook: "Metro 2033",
-    petName: "Melody"
-},
-{
+    petName: "Melody",
+  },
+  {
     name: "Andrea",
     surname: "Schiariti",
     age: 33,
@@ -33,17 +34,35 @@ const team1 = [{
     favoriteFilm: "Mrs Doubtfire",
     favoriteBook: "The Stone Monkey",
     petName: "Guy",
- },
- {
+  },
+  {
     name: "zain",
     surname: "ahmed",
-    age: 27,    
+    age: 27,
     city: "Bologna",
     hobby: "Games",
     favoriteFood: "Pizza",
     favoriteVideoGame: "LoL",
     favoriteFilm: "",
     favoriteBook: "",
-    petName: ""
-}
-]
+    petName: "",
+  },
+];
+
+alphabeticSurname = [...team1];
+
+alphabeticSurname.sort((name1, name2) => {
+  let firstSurname = name1.surname.toUpperCase();
+  let secondSurname = name2.surname.toUpperCase();
+
+  if (firstSurname < secondSurname) {
+    return -1;
+  }
+  if (firstSurname > secondSurname) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
+console.log(alphabeticSurname)
