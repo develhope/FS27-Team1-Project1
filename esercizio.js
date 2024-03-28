371529ad978d18890afc82cc84baa4a08d198c9b
zain <zainahmed638@gmail.com>
2024-03-28 12:38:26 +0100

    creazione ex.js

--- esercizio.js
+++ esercizio.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+const newArr = [{
+    name: "zain",
+    surname: "ahmed",
+    age: 27,    
+    city: "Bologna",
+    hobby: "Games",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "LoL",
+    favoriteFilm: "",
+    favoriteBook: "",
+    petName: ""
+}] 
